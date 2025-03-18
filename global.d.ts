1b6591de2e0e261c0bcdec17b3f8d19ba2524d0e
MikaNvj <masivolamika@gmail.com>
2025-03-18 10:30:46 +0100

    feat:interface parking

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native-vector-icons/Ionicons' {
+    import { IconProps } from 'react-native-vector-icons/Icon';
+    import { Component } from 'react';
+    export default class Ionicons extends Component<IconProps> {}
+  }
+  
